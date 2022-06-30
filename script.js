82da85acbf224ddb476c21a126c131afd439a1d7
mehran khalili <me.dr7725@gmail.com>
2022-06-30 17:20:56 +0430

    modify confirm

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const tmpl = `
-<tr>
+<tr id ="tr\${id}">
 <td><img src="\${avatar}"></td>
 <td>\${id}</td>
 <td>\${name}</td>
@@ -23,7 +23,7 @@ data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">edit</button>
 const creatRow = async () => {
     const data = await getUsers()
     data.map((item) => {
-        $.tmpl(tmpl,item).appendTo('tbody')
+        $.tmpl(tmpl, item).appendTo('tbody')
     })
 }
 
@@ -36,14 +36,24 @@ const initializedEventListener = async () => {
             const profile = document.querySelector('.offcanvas-body > img')
             profile.src = `${tds[0].firstChild.src}`
             Array.from(form.elements).map((item, index) => {
-                if(index == form.elements.length - 1) return
-                item.value = tds[index+1].textContent
+                if (index == form.elements.length - 1) return
+                item.value = tds[index + 1].textContent
             })
-            
+
         } else if (event.target.classList.contains('delete')) {
 
         }
-        
+
+    })
+
+    $(".confirm").on('click', (event) => {
+        const form = document.forms[0]
+        const tr = document.querySelector(`#tr${form.id.value}`)
+        const tds = tr.querySelectorAll('td')
+        Array.from(form.elements).map((item, index) => {
+            if (index == form.elements.length - 1) return
+            tds[index + 1].textContent =item.value  
+        })
     })
 }
 
